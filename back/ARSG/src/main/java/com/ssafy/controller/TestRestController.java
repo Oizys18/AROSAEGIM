98816545c642a7a1abb071f6006ec81c31feaf0e
전경윤 <kingjky@naver.com>
2020-05-15 09:30:41 +0900

    Add ERD

--- back/ARSG/src/main/java/com/ssafy/controller/TestRestController.java
+++ back/ARSG/src/main/java/com/ssafy/controller/TestRestController.java
@@ -7,22 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.repositories.TestuserRepository;
 import com.ssafy.service.TestuserService;
+
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = {"*"}, maxAge = 6000)
 @RequestMapping("/")
 @RestController
 public class TestRestController {
-	@Autowired
-    private TestuserRepository userRepository;
 	@Autowired
 	private TestuserService userService;
 	
@@ -30,14 +25,12 @@ public class TestRestController {
 	public ResponseEntity<Map<String, Object>> handle(Exception e){
 		return handleFail(e.getMessage(), HttpStatus.OK);
 	}
-	
 	public ResponseEntity<Map<String, Object>> handleSuccess(Object data){
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("state", "ok");
 		resultMap.put("data", data);
 		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
 	}
-	
 	public ResponseEntity<Map<String, Object>> handleFail(Object data, HttpStatus state){
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("state", "fail");
@@ -45,30 +38,27 @@ public class TestRestController {
 		return new ResponseEntity<Map<String,Object>>(resultMap, state); 
 	}
 	
-//	@ApiOperation("post 요청에 관한 인삿말을 출력한다")
+	@ApiOperation("post 요청에 관한 인삿말을 출력한다")
 	@PostMapping("/hello")
 	public ResponseEntity<Map<String, Object>> getHello() throws Exception{
 		return handleSuccess("POST Hello World");
 	}
-	
-//	@ApiOperation("get 요청에 관한 인삿말을 출력한다")
+	@ApiOperation("get 요청에 관한 인삿말을 출력한다")
 	@GetMapping("/")
 	public ResponseEntity<Map<String, Object>> postHello() throws Exception{
 		return handleSuccess("GET Hello World");
 	}
-	
-//	@GetMapping("/user/{userId}")
-//	public ResponseEntity<Map<String, Object>> getTestuser(@PathVariable("userId") long userId) throws Exception{
-//		return handleSuccess(userRepository.findById(userId));
-//	}
+	@ApiOperation("userName으로 회원 정보 검색")
 	@GetMapping("/user/{userName}")
 	public ResponseEntity<Map<String, Object>> getTestuser(@PathVariable("userName") String userName) throws Exception{
 		return handleSuccess(userService.getUser(userName));
 	}
+	@ApiOperation("모든 회원 정보 List")
 	@GetMapping("/users")
 	public ResponseEntity<Map<String, Object>> getTestusers() throws Exception{
 		return handleSuccess(userService.getUsers());
 	}
+	@ApiOperation("모든 회원의 수")
 	@GetMapping("/usercount")
 	public ResponseEntity<Map<String, Object>> getTestuserCount() throws Exception{
 		return handleSuccess(userService.getUserCount());
