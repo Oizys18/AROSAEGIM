265d9f13ee940dd65bc4ee85c1bd77f8afb9a590
양찬우 <oizys18@gmail.com>
2020-05-18 18:22:32 +0900

    Mod storybook, Add button, Navbar Added storybook addon for source code Mod button style Add Navbar

--- front/front-react/src/components/common/buttons/DefaultButton.jsx
+++ front/front-react/src/components/common/buttons/DefaultButton.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-import "./DefaultButton.css"
+import styled from "styled-components";
+
+const DefaultBtn = styled.button`
+  color: black;
+  padding: 4px 8px 4px 8px;
+  border-radius: 0.4em;
+  font-size: 16px;
+  border-radius: 0.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  box-shadow: 2px 2px 4px grey;
+  background-color: ${(props) => props.color || "ghostwhite"};
+  &:hover {
+    background-color: lightgrey;
+  }
+  &:active {
+    background-color: grey;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
 class DefaultButton extends Component {
   render() {
     return (
-      <div>
-        <button className="defaultBtn"> {this.props.text}</button>
-      </div>
+      <DefaultBtn onClick={this.props.onClick}>{this.props.text}</DefaultBtn>
     );
   }
 }
