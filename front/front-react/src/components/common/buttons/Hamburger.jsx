79018a8fe99fdf01fea2586bef438d8b121c538a
SinJ <johankim27@gmail.com>
2020-05-18 12:44:05 +0900

    Mod Hamburger.jsx Add Hamburger story

--- front/front-react/src/components/common/buttons/Hamburger.jsx
+++ front/front-react/src/components/common/buttons/Hamburger.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { IconButton } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
 
 class Hamburger extends Component {
   render(){
     return(
-      <div>
-
-      </div>
+      <StHamburger>
+        <IconButton>
+          <Menu/>
+        </IconButton>
+      </StHamburger>
     )
   }
 } export default Hamburger;
+
+const StHamburger = styled.div``
---
+++ front/front-react/src/components/common/buttons/Hamburger.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hamburger from './Hamburger';
+
+
+export default {
+  component: Hamburger,
+  title: 'Button',
+};
+
+export const hamburger = () => <Hamburger/>
