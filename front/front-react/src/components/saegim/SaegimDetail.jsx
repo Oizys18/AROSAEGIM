d149f58bfffc309e811d634996f4312e1a7f7d63
Solji Choi <bssj9307@gmail.com>
2020-05-18 21:23:09 +0900

    Add SaegimList, SaegimDetail

---
+++ front/front-react/src/components/saegim/SaegimDetail.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class SaegimDetail extends Component {
+  render() {
+    return (
+      <div>
+        상세보기
+      </div>
+    );
+  }
+}
+
+export default SaegimDetail;
---
+++ front/front-react/src/components/saegim/SaegimList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class SaegimList extends Component {
+  render() {
+    return (
+      <div>
+        새김 목록
+      </div>
+    );
+  }
+}
+
+export default SaegimList;
