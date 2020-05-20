f69ea9c670b5a4ffa96bfec6fb4d81f21f18a3e6
김건호 <mapthesoul99@naver.com>
2020-05-20 13:17:28 +0900

    Mod map : apply prettify

--- front/front-react/src/components/common/map/Map.jsx
+++ front/front-react/src/components/common/map/Map.jsx
@@ -1,8 +1,8 @@
 /*global kakao*/
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import DefaultButton from '../buttons/DefaultButton';
-import MapItem from './MapItem';
+import React, { Component } from "react";
+import styled from "styled-components";
+import DefaultButton from "../buttons/DefaultButton";
+import MapItem from "./MapItem";
 
 class Map extends Component {
   constructor() {
@@ -23,7 +23,8 @@ class Map extends Component {
     // load kakao api
     const script = document.createElement("script");
     script.async = true;
-    script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=78ea54eeb30cab7c0130ac4ab15d3939&autoload=false";
+    script.src =
+      "https://dapi.kakao.com/v2/maps/sdk.js?appkey=78ea54eeb30cab7c0130ac4ab15d3939&autoload=false";
     document.head.appendChild(script);
 
     // initialize state after script is loaded
@@ -31,31 +32,39 @@ class Map extends Component {
       kakao.maps.load(() => {
         let container = document.getElementById("Mymap");
         let options = {
-          center: new kakao.maps.LatLng(33.450701, 126.570667),// 37.506502, 127.053617),
+          center: new kakao.maps.LatLng(33.450701, 126.570667), // 37.506502, 127.053617),
           level: this.state.level,
         };
 
         this.map = new window.kakao.maps.Map(container, options);
 
         this.initializeOverlay();
-      })
-    }
+      });
+    };
   }
 
   initializeOverlay = () => {
-    let refArr = []
+    let refArr = [];
     this.setState({
       items: dummyItems,
       jsxItems: dummyItems.map((el, index) => {
         const itemRef = React.createRef();
-        const item = <MapItem item={el} key={index} map={this.map} selectItem={this.selectItem} ref={itemRef} />
-        refArr.push(itemRef)
-        return item
-      })
+        const item = (
+          <MapItem
+            item={el}
+            key={index}
+            map={this.map}
+            selectItem={this.selectItem}
+            ref={itemRef}
+          />
+        );
+        refArr.push(itemRef);
+        return item;
+      }),
     });
 
     this.refs = refArr;
-  }
+  };
 
   toggleRoadView = (event) => {
     if (this.state.roadView) {
@@ -71,7 +80,7 @@ class Map extends Component {
         roadView: true,
       });
     }
-  }
+  };
 
   // TODO : basic map methods
   // 3. go to DetailView (route)
@@ -80,45 +89,56 @@ class Map extends Component {
   handleZoomIn = (event) => {
     const currentLevel = this.map.getLevel();
     this.map.setLevel(currentLevel - 1);
-    this.setState({...this.state, level: currentLevel - 1});
-  }
+    this.setState({ ...this.state, level: currentLevel - 1 });
+  };
 
   handleZoomOut = (event) => {
     const currentLevel = this.map.getLevel();
     this.map.setLevel(currentLevel + 1);
-    this.setState({...this.state, level: currentLevel + 1});
-  }
+    this.setState({ ...this.state, level: currentLevel + 1 });
+  };
 
-  handleDetailView = (event) => {}
+  handleDetailView = (event) => {};
 
-  handleRoadView = (event) => {}
+  handleRoadView = (event) => {};
 
   showItem = (event) => {
-    this.refs.forEach((el)=>el.current.showItem())
-  }
+    this.refs.forEach((el) => el.current.showItem());
+  };
 
   hideItem = (event) => {
-    this.refs.forEach((el)=>el.current.hideItem())
-  }
+    this.refs.forEach((el) => el.current.hideItem());
+  };
 
   selectItem = (item) => {
     this.setState({
-      selected: JSON.stringify(item)
-    })
-  }
+      selected: JSON.stringify(item),
+    });
+  };
 
   render() {
     return (
       <MapContainer>
         <h1>지도컴포넌트</h1>
-        <DefaultButton text={this.state.roadView? "로드뷰 켜짐" : "로드뷰 꺼짐"} onClick={this.toggleRoadView}></DefaultButton>
-        <DefaultButton text="map level +" onClick={this.handleZoomIn}></DefaultButton>
-        <DefaultButton text="map level -" onClick={this.handleZoomOut}></DefaultButton>
+        <DefaultButton
+          text={this.state.roadView ? "로드뷰 켜짐" : "로드뷰 꺼짐"}
+          onClick={this.toggleRoadView}
+        ></DefaultButton>
+        <DefaultButton
+          text="map level +"
+          onClick={this.handleZoomIn}
+        ></DefaultButton>
+        <DefaultButton
+          text="map level -"
+          onClick={this.handleZoomOut}
+        ></DefaultButton>
         <DefaultButton text="show item" onClick={this.showItem}></DefaultButton>
         <DefaultButton text="hide item" onClick={this.hideItem}></DefaultButton>
-        <div>roadview status : {this.state.roadView? "ON" : "OFF"}</div>
+        <div>roadview status : {this.state.roadView ? "ON" : "OFF"}</div>
         <div>current map level: {this.state.level}</div>
-        <div>clicked Item : {this.state.selected ? this.state.selected : "none"}</div>
+        <div>
+          clicked Item : {this.state.selected ? this.state.selected : "none"}
+        </div>
         <MapContents id="Mymap"></MapContents>
         {this.state.jsxItems}
       </MapContainer>
@@ -129,30 +149,30 @@ class Map extends Component {
 const MapContainer = styled.div`
   width: 300px;
   height: 300px;
-`
+`;
 
 const MapContents = styled.div`
   width: 100%;
   height: 100%;
-`
+`;
 
 export default Map;
 
 const dummyItems = [
   {
-      title: '카카오', 
-      latlng: [33.450705, 126.570677]
+    title: "카카오",
+    latlng: [33.450705, 126.570677],
   },
   {
-      title: '생태연못', 
-      latlng: [33.450936, 126.569477]
+    title: "생태연못",
+    latlng: [33.450936, 126.569477],
   },
   {
-      title: '텃밭', 
-      latlng: [33.450879, 126.569940]
+    title: "텃밭",
+    latlng: [33.450879, 126.56994],
   },
   {
-      title: '근린공원',
-      latlng: [33.451393, 126.570738]
+    title: "근린공원",
+    latlng: [33.451393, 126.570738],
   },
 ];
--- front/front-react/src/components/common/map/MapItem.jsx
+++ front/front-react/src/components/common/map/MapItem.jsx
@@ -1,30 +1,30 @@
 /*global kakao*/
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import imgLeft from '../../../assets/balloon/balloon-left@2x.png';
-import imgMiddle from '../../../assets/balloon/balloon-middle@2x.png';
-import imgRight from '../../../assets/balloon/balloon-right@2x.png';
+import React, { Component } from "react";
+import styled from "styled-components";
+import imgLeft from "../../../assets/balloon/balloon-left@2x.png";
+import imgMiddle from "../../../assets/balloon/balloon-middle@2x.png";
+import imgRight from "../../../assets/balloon/balloon-right@2x.png";
 
 class MapItem extends Component {
   constructor(props) {
     super(props);
     this.myRef = React.createRef();
     this.state = {
-      item: {title: null, latlng: [null, null]},
-    }
+      item: { title: null, latlng: [null, null] },
+    };
   }
   componentDidMount() {
     this.setState({
-      item: this.props.item
-    })
+      item: this.props.item,
+    });
   }
-  
+
   componentDidUpdate() {
-    console.log(this.props.map)
+    console.log(this.props.map);
 
     if (!!this.props.map) {
       // do something
-      console.log(this.myRef)
+      console.log(this.myRef);
       this.showOnMap();
     }
   }
@@ -32,30 +32,33 @@ class MapItem extends Component {
   // initial map overlay
   showOnMap = () => {
     const customOverlay = new kakao.maps.CustomOverlay({
-      position: new kakao.maps.LatLng(this.state.item.latlng[0],this.state.item.latlng[1]),
+      position: new kakao.maps.LatLng(
+        this.state.item.latlng[0],
+        this.state.item.latlng[1]
+      ),
       content: this.myRef.current,
       yAnchor: 1,
       clickable: true,
     });
     customOverlay.setMap(this.props.map);
-    this.customOverlay = customOverlay
-  }
+    this.customOverlay = customOverlay;
+  };
 
   // deliver state item to parent
   clickEvent = (e) => {
     e.preventDefault();
-    this.props.selectItem(this.state.item)
-  }
+    this.props.selectItem(this.state.item);
+  };
 
   // show on map
   showItem = () => {
-    this.customOverlay.setMap(this.props.map)
-  }
+    this.customOverlay.setMap(this.props.map);
+  };
 
   // hide from map
   hideItem = () => {
-    this.customOverlay.setMap(null)
-  }
+    this.customOverlay.setMap(null);
+  };
 
   render() {
     return (
@@ -66,7 +69,7 @@ class MapItem extends Component {
         </ItemMiddle>
         <ItemRight />
       </ItemContainer>
-    )
+    );
   }
 }
 
@@ -76,14 +79,14 @@ const ItemContainer = styled.div`
   height: 30px;
   display: flex;
   margin-top: 15px;
-`
+`;
 
 const ItemLeft = styled.div`
   height: 20px;
   width: 7px;
   background-image: url(${imgLeft});
   background-size: contain;
-`
+`;
 
 const ItemMiddle = styled.div`
   height: 20px;
@@ -92,23 +95,23 @@ const ItemMiddle = styled.div`
   background-size: contain;
   padding-left: 4px;
   margin-right: -2px;
-`
+`;
 
 const ItemRight = styled.div`
   height: 20px;
   width: 13px;
   background-image: url(${imgRight});
   background-size: contain;
-`
+`;
 
 const TextMiddle = styled.div`
   position: relative;
   bottom: 10px;
   border-radius: 2px;
   padding: 2px;
-  color: #20AD77;
-  background: rgba(255,255,255,0.9);
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  color: #20ad77;
+  background: rgba(255, 255, 255, 0.9);
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
-`
+`;
