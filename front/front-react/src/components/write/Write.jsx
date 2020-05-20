ee3a8bf49890b22c3308dba630f95953ea5a9677
양찬우 <oizys18@gmail.com>
2020-05-20 16:11:47 +0900

    Mod Write - Add icons

--- front/front-react/src/components/write/Write.jsx
+++ front/front-react/src/components/write/Write.jsx
@@ -2,20 +2,58 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import LockOpenOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import MapIcon from "@material-ui/icons/Map";
 import { IconButton } from "@material-ui/core";
+import CtoW from "../../apis/w3w";
+import Input from '@material-ui/core/Input';
 
 class Write extends Component {
-  state = {
-    locked: false,
+  constructor() {
+    super();
+    this.state = {
+      locked: false,
+      location: null,
+      w3w: null,
+    };
+  }
+  getLocation = () => {
+    if (navigator.geolocation) {
+      // GPS를 지원
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          alert([position.coords.latitude + " " + position.coords.longitude]);
+          this.setState({
+            location: [position.coords.latitude, position.coords.longitude],
+          });
+        },
+        function(error) {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity,
+        }
+      );
+    } else {
+      alert("GPS를 지원하지 않습니다");
+    }
   };
 
-  LockOrUnlock = () => {
+  lockOrUnlock = () => {
     if (this.state.locked) {
       this.setState({ locked: false });
     } else {
       this.setState({ locked: true });
     }
   };
+  getLoc = async (location) => {
+    const getW3W = await CtoW(location[0], location[1]);
+    this.setState({ w3w: getW3W });
+    console.log(this.state.w3w);
+  };
+
   render() {
     const locked = this.state.locked;
     let icon;
@@ -25,15 +63,29 @@ class Write extends Component {
       icon = <LockOpenOutlinedIcon />;
     }
 
+    // const w3w = this.state.w3w;
+    // let loc;
+    // if (w3w) {
+    //   loc = <div>{w3w}</div>;
+    // } else {
+    //   loc = <div>hello</div>;
+    // }
+
     return (
       <Wrapper>
         <Container>
-          <Lock onClick={this.LockOrUnlock}>{icon}</Lock>
-          <Text>당신의 추억을 새겨보세요.</Text>
-          <Location>
-            <div>Your Location</div>
-            <div>Your Location</div>
-          </Location>
+          <Lock onClick={this.lockOrUnlock}>{icon}</Lock>
+          <Text>
+            <Input placeholder="당신의 추억을 새겨보세요"/>
+            </Text>
+          <Addition>
+            <Map onClick={this.getLocation}>
+              <MapIcon />
+            </Map>
+            <Tag onClick={() => alert("태그 곧 넣을게요ㅠ")}>
+              <LocalOfferIcon />
+            </Tag>
+          </Addition>
         </Container>
       </Wrapper>
     );
@@ -60,8 +112,8 @@ const Container = styled.div`
   width: 84vw;
   height: 48vh;
   display: grid;
-  grid-template-rows: 1fr 5fr;
-  grid-template-columns: 1fr 5fr 1fr;
+  grid-template-rows: 1fr 4fr 2fr;
+  grid-template-columns: 1fr 8fr 1fr;
   justify-content: center;
   flex-direction: column;
   align-items: center;
@@ -81,10 +133,29 @@ const Text = styled.div`
   justify-content: center;
   align-items: center;
   display: flex;
-  grid-column: 2 / 2;
+  grid-column: 1 / 4;
   grid-row: 2 / 2;
 `;
-const Location = styled.div`
-  grid-column: 1 / 3;
+const Addition = styled.div`
+  grid-column: 1 / 2;
   grid-row: 3 / 3;
+`;
+
+const Map = styled(IconButton)`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  background-color: transparent;
+  border: none;
+  outline: none;
+`;
+const Tag = styled(IconButton)`
+  margin: none;
+  padding: none;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  background-color: transparent;
+  border: none;
+  outline: none;
 `;
