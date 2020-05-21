f4861eaf84fc53b3858211980c759e0ddd9d1d8b
양찬우 <oizys18@gmail.com>
2020-05-21 17:14:40 +0900

    Add text Input component

---
+++ front/front-react/src/components/common/inputs/TextInput.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import TextField from "@material-ui/core/TextField";
+class TextInput extends Component {
+  render() {
+    return (
+      <div>
+        <CustomInput
+          placeholder="당신의 추억을 새겨주세요"
+          multiline
+          rows={5}
+          rowsMax={6}
+          margin="dense"
+          variant="outlined"
+          color="warning"
+        />
+      </div>
+    );
+  }
+}
+export default TextInput;
+
+const CustomInput = styled(TextField)`
+  width: 100%;
+  background-color: transparent;
+`;
