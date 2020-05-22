309144f5de64f9b5dec2210949b4b4cc612024bf
Solji Choi <bssj9307@gmail.com>
2020-05-22 11:49:54 +0900

    Mod App, BotNav, SaegimItem, Del Main mod router

--- front/front-react/src/App.jsx
+++ front/front-react/src/App.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import { Route, withRouter } from "react-router-dom";
-
 import { Storage } from "./storage/Storage";
 import TopBar from "./components/common/menus/TopBar";
 import SideMenu from "./components/common/menus/SideMenu";
 import BotNav from "./components/common/navbar/BotNav";
-import Main from "./components/main/Main";
 import Map from "./components/common/map/Map";
 import Write from "./components/write/Write";
 // import Auth from "./components/account/Auth";
@@ -22,7 +20,7 @@ class App extends Component {
 
       isLogin: false,
 
-      userInitPage: "/main",
+      userInitPage: "/list",
       curPage: this.props.location.pathname,
 
       sideMenu: false,
@@ -45,7 +43,8 @@ class App extends Component {
   };
 
   changePage = (e) => {
-    if (e.currentTarget.id === "write") {
+    if (e.currentTarget.id === "write"
+    ) {
       this.props.history.push(e.currentTarget.id);
     } else {
       this.props.history.replace(e.currentTarget.id);
@@ -56,9 +55,10 @@ class App extends Component {
     return (
       <Storage.Provider value={this.state}>
         {// 사이드메뉴랑, 상단바(햄버거), 하단네비는 그냥 조건부 렌더링으로 작성
-        (this.props.location.pathname === "/main" ||
-          this.props.location.pathname === "/map" ||
-          this.props.location.pathname === "/write") && (
+        ( this.props.location.pathname === "/map" ||
+          this.props.location.pathname === "/write" ||
+          this.props.location.pathname.includes("/list")
+        ) && (
           <>
             <TopBar on={this.state.sideMenu} toggle={this.toggleSideMenu} />
             <SideMenu
@@ -70,15 +70,14 @@ class App extends Component {
           </>
         )}
 
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={SaegimListPage} />
         {/* <Route path="/auth" component={Auth} /> */}
-        <Route path="/main" component={Main} />
+        <Route exact path="/list" component={SaegimListPage} />
         <Route path="/map" component={Map} />
         <Route path="/write" component={Write} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route exact path="/saegim" component={SaegimListPage} />
-        <Route exact path="/saegim/:id" component={SaegimDetail} />
+        <Route exact path="/list/:id" component={SaegimDetail} />
       </Storage.Provider>
     );
   }
