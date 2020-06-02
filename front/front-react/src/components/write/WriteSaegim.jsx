e7b016a008324aec5c5867c5a90ddf43a45dbd01
양찬우 <oizys18@gmail.com>
2020-06-02 21:38:11 +0900

    Mod write style

--- front/front-react/src/components/write/WriteSaegim.jsx
+++ front/front-react/src/components/write/WriteSaegim.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import MapIcon from "@material-ui/icons/Map";
 import { IconButton } from "@material-ui/core";
 import TextInput from "../common/inputs/TextInput";
 import Chip from "../common/chip/Chip";
-import DefaultButton from "../common/buttons/DefaultButton";
 import CreateIcon from "@material-ui/icons/Create";
 import CtoW from "../../apis/w3w";
 import Switch from "../common/switch/Switch";
@@ -13,6 +11,7 @@ import axios from "axios";
 import PhotoIcon from "@material-ui/icons/AddPhotoAlternate";
 import { getUserByEmail } from "../../apis/AccountAPI";
 import { Storage } from "../../storage/Storage";
+import Popover from "@material-ui/core/Popover";
 
 class WriteSaegim extends Component {
   constructor(props) {
@@ -28,22 +27,24 @@ class WriteSaegim extends Component {
       error: 0,
       userInfo: {},
       imgBase64: [],
+      open: false,
     };
     this.inputReference = React.createRef();
   }
 
   fileUploadAction = () => this.inputReference.current.click();
-
   fileUploadInputChange = async (e) => {
     e.preventDefault();
-    const _reader = new FileReader();
-    const _imgFile = e.target.files[0];
-    _reader.readAsDataURL(_imgFile);
-    _reader.onloadend = () => {
-      this.setState({
-        imgBase64: this.state.imgBase64.concat(_reader.result),
-      });
-    };
+    for (let i = 0; i < e.target.files.length; i++) {
+      let _reader = new FileReader();
+      let _imgFile = e.target.files[i];
+      _reader.readAsDataURL(_imgFile);
+      _reader.onloadend = () => {
+        this.setState({
+          imgBase64: this.state.imgBase64.concat(_reader.result),
+        });
+      };
+    }
   };
 
   handleChange = (data) => {
@@ -100,6 +101,7 @@ class WriteSaegim extends Component {
       userName: this.state.userInfo.name,
       w3w: this.state.w3w,
     };
+    console.log(data);
     if (this.state.text) {
       axios
         .post("https://k02a2051.p.ssafy.io/api/saegims/", data)
@@ -124,10 +126,6 @@ class WriteSaegim extends Component {
       this.setState({ locked: 1 });
     }
   };
-  createTag = (newTag) => {
-    this.setState({ tags: this.state.tags.concat(newTag) });
-  };
-
   getUserInfo = async () => {
     // const _email = localStorage.getItem('ARSG email')
     // this.setState({
@@ -137,13 +135,26 @@ class WriteSaegim extends Component {
       userInfo: this.context.userInfo,
     });
   };
+  // Tag 작성 및 Tag popover
+  createTag = (newTag) => {
+    this.setState({ tags: this.state.tags.concat(newTag) });
+  };
+  handleTag = () => {
+    console.log(this.state.tagModal);
+    if (this.state.tagModal) {
+      this.setState({ tagModal: false });
+    } else {
+      this.setState({ tagModal: true });
+    }
+  };
 
   render() {
+
     const ErrorMsg = () => {
       if (this.state.error) {
         return <Error>텍스트를 입력해주세요!</Error>;
       } else {
-        return <div></div>;
+        return <Error>　</Error>;
       }
     };
     return (
@@ -169,7 +180,7 @@ class WriteSaegim extends Component {
               labelText={this.state.locked ? "비공개" : "공개"}
               labelPlacement="start"
             />
-            <Tag onClick={() => alert("태그 곧 넣을게요ㅠ")}>
+            <Tag>
               {this.state.tags.map((tag, i) => {
                 return <Chip size="small" text={tag} key={i} />;
               })}
@@ -181,6 +192,7 @@ class WriteSaegim extends Component {
             type="file"
             hidden
             id="imgUpload"
+            multiple
             accept="image/*"
             ref={this.inputReference}
             onChange={this.fileUploadInputChange}
@@ -188,23 +200,59 @@ class WriteSaegim extends Component {
           <CreateImg onClick={this.fileUploadAction}>
             <PhotoIcon />
           </CreateImg>
-          <CreateTag onClick={() => this.createTag("hello")}>
+          <CreateTag onClick={this.openPop}>
+            <Popover
+              id="tag-popover"
+              open={this.state.popOpen}
+              // anchorEl={anchorEl}
+              onClose={this.popClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              The content of the Popover.
+            </Popover>
             <LocalOfferIcon />
           </CreateTag>
-          <CreateButton onClick={() => this.writePost()}>
+          <CreateButton onClick={this.writePost}>
             <CreateIcon />
           </CreateButton>
         </CreateWrapper>
+        <ImageWrapper>
+          {this.state.imgBase64.map((img, i) => {
+            return <ImgBalloon src={img} key={i} />;
+          })}
+        </ImageWrapper>
       </Wrapper>
     );
   }
 }
 export default WriteSaegim;
 WriteSaegim.contextType = Storage;
-
+const ImgBalloon = styled.img`
+  width: 50px;
+  height: 50px;
+  border: 2px solid white;
+  border-radius: 24px;
+  margin: 0.5vw;
+`;
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  align-items: center;
+  width: 80vw;
+  margin-top: 4vh;
+`;
 const Error = styled.div`
   color: red;
   font-size: 10px;
+  position: relative;
 `;
 
 const Wrapper = styled.div`
@@ -233,7 +281,7 @@ const Top = styled.div`
 `;
 
 const Bottom = styled.div`
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   display: flex;
 `;
@@ -245,22 +293,35 @@ const CreateWrapper = styled.div`
   align-items: center;
   display: flex;
   margin-top: 10px;
+  border-radius: 16px;
 `;
 const CreateTag = styled(IconButton)`
-  color: default;
   background: white;
   padding: 0.25em;
   margin-right: 10px;
+  &:focus {
+    background: lightgrey;
+  }
 `;
+
 const CreateButton = styled(IconButton)`
   background: white;
   padding: 0.25em;
+  &:focus {
+    background: lightgrey;
+  }
 `;
 
 const CreateImg = styled(IconButton)`
   background: white;
   padding: 0.25em;
   margin-right: 10px;
+  &:focus {
+    background: lightgrey;
+  }
+  &:active {
+    background: lightgrey;
+  }
 `;
 
 const Tag = styled(IconButton)`
