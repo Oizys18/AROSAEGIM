a8cc20437d9d0dc71642fa213def04befaeee214
Solji Choi <bssj9307@gmail.com>
2020-06-05 12:56:24 +0900

    Mod Card resolve card conflict

--- front/front-react/src/components/common/cards/Card.jsx
+++ front/front-react/src/components/common/cards/Card.jsx
@@ -12,9 +12,9 @@ export default Card;
 const StCard = styled.div`
   border-radius: 16px;
   background: ${props => props.color};
-  box-shadow: 16px 16px 32px rgba(77,77,77,0.5), -16px -16px 32px rgba(89,89,89,0.5);  padding: 16px 24px 24px 16px;
+  box-shadow: 16px 16px 32px rgba(77,77,77,0.5), -16px -16px 32px rgba(89,89,89,0.5);
+  padding: 16px 24px 24px 16px;
   margin: 0px 16px 0px 16px;
-  // animation: {color_change} 3s ease alternate;
 `;
 
 
