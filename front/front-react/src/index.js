083d1b35fc8188a2b66247528a48531bf26e4b75
최솔지 <bssj9307@gmail.com>
2020-06-06 16:00:49 +0000

    Mod index.js

--- front/front-react/src/index.js
+++ front/front-react/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { StylesProvider, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
