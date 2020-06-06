843c1ddfe3b2f030dec984be3081ea251fe482be
Solji Choi <bssj9307@gmail.com>
2020-06-07 00:57:00 +0900

    Mod index.js Add default font (material ui)

--- front/front-react/src/index.js
+++ front/front-react/src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Noto Serif KR, serif',
+  },
+});
 
 ReactDOM.render(
   // material-ui 와 styled-components 같이 사용해야할때 <StylesProvider injectFirst>를 선언해줘야 함.
+  <MuiThemeProvider theme={theme}>
   <StylesProvider injectFirst>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App/>
-      </React.StrictMode>
-    </BrowserRouter>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App/>
+        </React.StrictMode>
+      </BrowserRouter>
   </StylesProvider>
+  </MuiThemeProvider>
+
   , document.getElementById('root')
 );
 
