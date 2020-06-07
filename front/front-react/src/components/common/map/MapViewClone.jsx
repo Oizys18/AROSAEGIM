2abf537824a0e80ecfcd22ca3aa08e31993f31bb
김건호 <mapthesoul99@naver.com>
2020-06-07 20:02:07 +0900

    Delete Unnecessary lines

--- front/front-react/src/components/common/map/MapViewClone.jsx
+++ front/front-react/src/components/common/map/MapViewClone.jsx
@@ -18,7 +18,6 @@ class MapView extends Component {
     this.state = {
       items: this.props.items,
       mv: null,
-      mapProjection: null,
       userMarker: null,
       
       write: {
@@ -48,37 +47,10 @@ class MapView extends Component {
         this.setState({w3w: w3w.data.words})
       }
     }
-
-    // // 현위치로 가기 눌렀을 때
-    // if(prevProps.mapCenter !== this.props.mapCenter && this.props.mapCenter === this.props.userCenter){
-    //   this.state.mv.panTo(this.props.mapCenter);
-    //   // console.log('map is in user center')
-    //   (this.state.userMarker && this.state.userMarker.setPosition(this.props.userCenter));
-    // }
-
-    // // 페이지 전환시 지도 중심, 레벨 유지
-    // if(prevProps.mapCenter !== this.props.mapCenter && 
-    //   this.context.curMapCenter === this.props.mapCenter && 
-    //   this.props.mapCenter !== this.props.userCenter){
-    //   this.state.mv.panTo(this.props.mapCenter);
-    //   this.state.mv.setLevel(this.props.mapLevel);
-    // }
-
-
-    // (!!this.props.userCenter && this.props.usingUserCenter && !this.state.userMarker && this.showUserCenter())
-    // if (prevProps.usingUserCenter !== this.props.usingUserCenter){console.log(prevProps, this.props, this.state)}
-    // if (prevProps.usingUserCenter !== this.props.usingUserCenter && this.props.usingUserCenter && !!this.state.userMarker) {
-    //   
-    //   this.state.mv.panTo(this.props.userCenter)
-    //   // MM.panTo(this.state.mv, this.props.userCenter.getLat(), this.props.userCenter.getLng())
-    // }
-
   }
 
   componentWillUnmount(){
-    // kakao.maps.event.removeListener(this.state.mv, "zoom_changed", this.changeLvCt)
     kakao.maps.event.removeListener(this.state.mv, "center_changed", this.handleCenterChange)
-    // kakao.maps.event.removeListener(this.state.mv, "zoom_changed", this.changeLv)
     kakao.maps.event.removeListener(this.state.mv, "dragstart", this.handleDragStart)
     kakao.maps.event.removeListener(this.state.mv, "dragend", this.handleDragEnd)
   }
@@ -91,28 +63,12 @@ class MapView extends Component {
       level: this.props.mapLevel,
     }
     const _mapView = new kakao.maps.Map(_cont, _options)
-    // kakao.maps.event.addListener(_mapView, "zoom_changed", this.changeLvCt)
-    // kakao.maps.event.addListener(_mapView, "center_changed", this.changeLvCt)
-    // kakao.maps.event.addListener(_mapView, "zoom_changed", this.changeLv)
     kakao.maps.event.addListener(_mapView, "center_changed", this.handleCenterChange)
     kakao.maps.event.addListener(_mapView, "dragstart", this.handleDragStart)
     kakao.maps.event.addListener(_mapView, "dragend", this.handleDragEnd)
 
-    const _mapProjection = _mapView.getProjection()
-
-    // const _clusterer = new kakao.maps.MarkerClusterer({
-    //   map: _mapView, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
-    //   averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
-    //   minLevel: 1, // 클러스터 할 최소 지도 레벨 
-    //   disableClickZoom: true // 클러스터 마커를 클릭했을 때 지도가 확대되지 않도록 설정한다
-    // });
-
-    // kakao.maps.event.addListener(_clusterer, 'clusterclick', this.handleCluster);
-
     this.setState({
       mv: _mapView,
-      // mapProjection: _mapProjection,
-      // clusterer: _clusterer,
     })
   }
 
@@ -138,19 +94,12 @@ class MapView extends Component {
   }
 
   handleDragStart = () => {
-    // (this.state.selected.status && this.closeItem());
-    // (this.props.usingUserCenter && this.props.unsetUsingUserCenter());
-    // (this.state.selectedList.status && this.unsetSelectedList());
-    // this.closeItem();
     this.setState({write: {moving: true}})
   };
 
   handleDragEnd = () => {
     this.setState({write: {moving: false}})
     this.props.changeMapCenter(this.state.mv.getCenter())
-    // this.fetchItem();
-    // (this.state.selected.status && this.closeItem());
-    // (this.state.usingUserCenter && this.setState({usingUserCenter: false}));
   };
 
   overlayCurPosition = () => {
@@ -198,14 +147,6 @@ class MapView extends Component {
       <>
         <StView id="mapView" hidden={this.props.hide}>
         </StView>
-        {this.props.status === "list" && 
-        this.state.selectedList.status && 
-          <StListCont>
-            <StList>
-              {this.overlaySelectedList()}
-            </StList>
-          </StListCont>
-        }
         {this.props.status === "write" && !!this.state.w3w &&
           <StTextWrapper>
             <Chip color="primary" size="medium" label={this.state.w3w} icon={<LocationOnIcon />}/>
@@ -228,29 +169,6 @@ const StView = styled.div`
   width: 100%;
   height: 100%;
 `;
-
-const StListCont = styled.div`
-  position: absolute;
-  width: auto;
-  height: auto;
-  overflow-y: auto;
-  max-height: 65%;
-  max-width: 60%;
-  z-index: 15;
-  top: 50%;
-  right: 0px;
-  padding-right: 8px;
-  padding-top: 16px;
-  padding-bottom: 16px;
-  transform: translateY(-40%);
-  /* background: linear-gradient(0.25turn, rgba(255,255,255,0) 80%, #838e83 ); */
-`
-
-const StList = styled.div`
-  display: flex;
-  flex-direction: column-reverse;
-  align-items: flex-end;
-`
 
 const StButtonWrapper = styled.div`
   position: absolute;
