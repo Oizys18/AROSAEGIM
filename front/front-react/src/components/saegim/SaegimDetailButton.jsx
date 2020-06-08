d884d13b76a215612ccbdc17491e521822e8a6e0
Solji Choi <bssj9307@gmail.com>
2020-06-08 11:00:20 +0900

    Mod SaegimDetailButton 코드 정리

--- front/front-react/src/components/saegim/SaegimDetailButton.jsx
+++ front/front-react/src/components/saegim/SaegimDetailButton.jsx
@@ -1,30 +1,9 @@
 import React, { Component } from "react";
 import { Storage } from "../../storage/Storage";
 import styled from "styled-components";
-import { Menu, MenuItem, Fab } from "@material-ui/core";
-import { MoreVert } from "@material-ui/icons";
 import { withRouter } from "react-router-dom";
-import { FlexRow } from "../../styles/DispFlex";
 
 class SaegimDetailButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vertOpen: null,
-    }
-  }
-
-  handleClick = (e) => {
-    this.setState({
-      vertOpen: e.currentTarget
-    })
-  }
-
-  handleClose = () => {
-    this.setState({
-      vertOpen: null
-    })
-  }
 
   delSaegim = () => {
     this.context.popModal('새김을 삭제하시겠습니까?', 'delSaegim', 'confirm')
@@ -34,8 +13,8 @@ class SaegimDetailButton extends Component {
   render() {
     return(
       <StDetail>
-        <div onClick={this.handleClose} style={{ color: 'gray' }}>
-            수정
+        <div style={{ color: 'gray' }}>
+          수정
         </div>
         <div onClick={this.delSaegim}>
           삭제
